3f891f26772f0887d66f605211e2b38caa77e086
Arkhalisal <wf51182474@gmail.com>
2024-10-18 16:17:31 +0800

    update ver 1.3

--- src/component/ShowingStopsAndTime.js
+++ src/component/ShowingStopsAndTime.js
@@ -19,30 +19,33 @@ function ShowingStopsAndTime({ choosedRoute }) {
   if (!choosedRoute.route) return null;
 
   return (
-    <div className="busDataContainer">
-      {busStopData.map((busStop, index) => {
-        return (
-          <div className="buses" key={index}>
-            <div>{`${index + 1}. ` + busStop[0].name}</div>
-            <div className="firstBus">
-              <div className="busTime">
-                {!busStop[0].time ? "No Bus Is Available" : busStop[0].time}
+    <div className="allDataContainer">
+      <div className="routeInfo">{`${choosedRoute.route} - ${choosedRoute.orig_tc} to ${choosedRoute.dest_tc}`}</div>
+      <div className="busDataContainer">
+        {busStopData.map((busStop, index) => {
+          return (
+            <div className="buses" key={index}>
+              <div>{`${index + 1}. ` + busStop[0].name}</div>
+              <div className="firstBus">
+                <div className="busTime">
+                  {!busStop[0].time ? "No Bus Is Available" : busStop[0].time}
+                </div>
+                <div className="busTrips">
+                  {!busStop[0].trips ? "" : busStop[0].trips}
+                </div>
               </div>
-              <div className="busTrips">
-                {!busStop[0].trips ? "" : busStop[0].trips}
+              <div className="secondBus">
+                <div className="busTime">
+                  {!busStop[1].time ? "No Bus Is Available" : busStop[1].time}
+                </div>
+                <div className="busTrips">
+                  {!busStop[1].trips ? "" : busStop[1].trips}
+                </div>
               </div>
             </div>
-            <div className="secondBus">
-              <div className="busTime">
-                {!busStop[1].time ? "No Bus Is Available" : busStop[1].time}
-              </div>
-              <div className="busTrips">
-                {!busStop[1].trips ? "" : busStop[1].trips}
-              </div>
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
